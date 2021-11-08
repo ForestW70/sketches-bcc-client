ed62ef5c87a30906091a3e90c7a48c59ae4b9f7a
Forest wilson <hexaforest@gmail.com>
2021-11-08 17:49:39 -0500

    added tracklist generation from js.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,42 @@
-console.log('wddup')
+console.log('wddup');
 
-const nowPlaying = document.getElementById("nowPlaying")
-const trackListContainer = document.getElementById("trackList")
+const nowPlaying = document.getElementById("nowPlaying");
+const trackListContainer = document.getElementById("trackList");
 
+const trkList = [
+    "Hotmess",
+    "Quarantine (but it's ok)",
+    "Amanda",
+    "Atmosphere Interlude",
+    "Stickyhold",
+    "Stargate (reprise)",
+    "Rap",
+    "Dare2bare",
+    "Almostinato",
+    "27th&girard",
+    "5d",
+    "Probably Wrong",
+    "Dr. Balachander",
+    "Boc Interlude",
+    "Toothpicks",
+    "Pilotdrone",
+    "Faultline"
+];
+
+const bcc = [
+    {
+        trkNum: 1,
+        title: "Hotmess",
+        src: "./music/A1"
+    }
+]
+
+trkList.forEach(track => {
+    const btn = document.createElement("button");
+    btn.innerText = track;
+    btn.classList.add("track-select");
+    btn.addEventListener("click", () => {
+        console.log(track)
+    })
+    trackListContainer.appendChild(btn);
+})
