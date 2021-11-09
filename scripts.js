6b79e916881af41e452fa629a5f1d42c7c9c7b36
Forest wilson <hexaforest@gmail.com>
2021-11-09 12:33:15 -0500

    added more to song selecting functions. visible selected track, active tracks title is displayed.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ console.log('wddup');
 
 const nowPlaying = document.getElementById("nowPlaying");
 const trackListContainer = document.getElementById("trackList");
+const allSongsBtns = document.getElementsByClassName("track-select");
 
 const trkList = [
     "Hotmess",
@@ -31,12 +32,25 @@ const bcc = [
     }
 ]
 
+const setSelectedSong = (tracktitle) => {
+    nowPlaying.innerText = tracktitle
+}
+
+const btnArry = [];
+
 trkList.forEach(track => {
     const btn = document.createElement("button");
     btn.innerText = track;
     btn.classList.add("track-select");
+
     btn.addEventListener("click", () => {
-        console.log(track)
+        btnArry.forEach(button => button.id = "");
+        btn.id = "selectedSong";
+        setSelectedSong(track)
+        console.log(btn.id)
     })
+
+    btnArry.push(btn);
     trackListContainer.appendChild(btn);
+    // console.log(btnArry)
 })
