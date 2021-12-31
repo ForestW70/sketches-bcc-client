d5c83abac33707e7e987323a70483ab43eda37dc
Forest wilson <hexaforest@gmail.com>
2021-12-31 17:09:30 -0500

    added now playing display and source swap, music file organization.

--- scripts.js
+++ scripts.js
@@ -7,121 +7,39 @@ const sketches = [
             {
                 track: 'Quarantine (but its ok)',
                 length: '2:56',
-                url: '/music/quarantine.mp3',
+                url: 'quarantine',
             },
             {
                 track: '5d',
                 length: '3:01',
-                url: '/music/5d.mp3',
+                url: '5d',
             },
             {
                 track: 'Sticky Hold',
                 length: '1:27',
-                url: '/music/stickyhold.mp3',
+                url: 'sticky-hold',
             },
             {
                 track: 'Probably Wrong',
                 length: '3:16',
-                url: '/music/probablywrong.mp3',
+                url: 'probably-wrong',
             },
             {
                 track: 'Stargate',
                 length: '?:??',
-                url: '/music/stargateog.mp3',
+                url: 'stargate',
             },
             {
                 track: 'Rap (1896 mix)',
                 length: '0:56',
-                url: '/music/rap1896.mp3',
+                url: 'rap-1896',
             },
             {
                 track: 'Dare 2 Bare',
                 length: '2:53',
-                url: '/music/dare2bare.mp3',
+                url: 'dare-2-bare',
             },
 
-        ]
-    },
-    {
-        title: 'Sketches & Other Failures II',
-        artLink: 'https://picsum.photos/199',
-        trackList: [
-            {
-                track: 'Amanda Morph',
-                length: '',
-                url: '/music/',
-            },
-            {
-                track: 'Sara Tonin',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Bailey melody 07',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Oh techre!',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Go to feather',
-                length: '',
-                url: '',
-            },
-            {
-                track: '(I am aware of the) black box',
-                length: '',
-                url: '',
-            },
-            {
-                track: '1800harp',
-                length: '',
-                url: '',
-            },
-        ]
-    },
-    {
-        title: 'Sketches & Other Failures Part 3',
-        artLink: 'https://picsum.photos/198',
-        trackList: [
-            {
-                track: 'Froggy',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Hello grommet',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Live from the polls',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Case of the cookies',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Is it forever?',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'BBBB',
-                length: '',
-                url: '',
-            },
-            {
-                track: 'Silly Singing silicon',
-                length: '',
-                url: '',
-            },
         ]
     },
     {
@@ -130,37 +48,37 @@ const sketches = [
         trackList: [
             {
                 track: 'Amanda Alarm (wake up hunny)',
-                length: '',
-                url: '',
+                length: '1:56',
+                url: 'amanda-alarm',
             },
             {
                 track: 'Sweet Dreams',
-                length: '',
-                url: '',
+                length: '2:35',
+                url: 'sweet-dreams',
             },
             {
                 track: 'Doogood',
-                length: '',
-                url: '',
+                length: '1:32',
+                url: 'dogood',
             },
             {
                 track: 'Exploitable Resources',
-                length: '',
-                url: '',
+                length: '1:24',
+                url: 'resources',
             },
             {
                 track: 'Looking at a Screen',
-                length: '',
-                url: '',
+                length: '2:57',
+                url: 'looking-at-a-screen',
             },
             {
                 track: 'Gimp',
-                length: '',
-                url: '',
+                length: '2:31',
+                url: 'gimp',
             },
             {
                 track: 'It Is Forever',
-                length: '',
+                length: '?:??',
                 url: '',
             },
         ]
@@ -297,22 +215,128 @@ const sketches = [
             },
         ]
     },
+    {
+        title: 'Sketches & Other Failures II',
+        artLink: 'https://picsum.photos/199',
+        trackList: [
+            {
+                track: 'Amanda Morph',
+                length: '',
+                url: '/music/',
+            },
+            {
+                track: 'Sara Tonin',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Bailey melody 07',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Oh techre!',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Go to feather',
+                length: '',
+                url: '',
+            },
+            {
+                track: '(I am aware of the) black box',
+                length: '',
+                url: '',
+            },
+            {
+                track: '1800harp',
+                length: '',
+                url: '',
+            },
+        ]
+    },
+    {
+        title: 'Sketches & Other Failures Part 3',
+        artLink: 'https://picsum.photos/198',
+        trackList: [
+            {
+                track: 'Froggy',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Hello grommet',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Live from the polls',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Case of the cookies',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Is it forever?',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'BBBB',
+                length: '',
+                url: '',
+            },
+            {
+                track: 'Silly Singing silicon',
+                length: '',
+                url: '',
+            },
+        ]
+    },
 ]
 
+const audioPlayer = document.querySelector(".player");
 const nowPlaying = document.getElementById("nowPlaying");
 const discoContainer = document.getElementById("disco");
 
+const currArtistName = document.getElementById("artistName");
+const currTrackName = document.getElementById("trackName");
+const currTrackTime = document.getElementById("trackTime");
+const currAlbumName = document.getElementById("albumName");
+const currAlbumPic = document.getElementById("albumPic");
+
 
 const handleTrackSelect = (e) => {
+    // reset now playing
+    const filterPlayer = (songRow) => {
+        currArtistName.innerText = ""
+        currTrackName.innerText = ""
+        currTrackTime.innerText = ""
+        currAlbumName.innerText = ""
+        audioPlayer.src = "";
+        currAlbumPic.src = ""
+
+        currArtistName.innerText = "Lukasz Mauro"
+        currTrackName.innerText = songRow.querySelector(".album-track").innerText;
+        currTrackTime.innerText = songRow.querySelector(".track-time").innerText;
+        currAlbumName.innerText = songRow.dataset.epname;
+        currAlbumPic.src = songRow.dataset.albumurl
+
+    }
+
     // track info
-    nowPlaying.innerHTML = ""
-    const dispTrack = document.createElement("h2");
     const target = e.currentTarget;
-    const onNow = target.querySelector(".album-track");
-    dispTrack.innerText = onNow.innerText;
-    nowPlaying.appendChild(dispTrack);
+    filterPlayer(target);
 
     // load music
+    const songUrl = target.dataset.url
+    const loadedTrack = `/music/${songUrl}.mp3`
+    audioPlayer.src = loadedTrack;
+    // console.log(loadedTrack);
 
 }
 
@@ -330,6 +354,8 @@ const mapThruAlbums = () => {
         trackContainer.classList.add("track-container")
         // 
 
+        const epName = album.title;
+        const epArt = album.artLink;
         albumTitle.innerText = album.title;
         albumArt.src = album.artLink;
 
@@ -340,6 +366,10 @@ const mapThruAlbums = () => {
             const tTime = document.createElement("span");
             const tNum = document.createElement("span");
 
+            trackRow.dataset.url = idvTrack.url;
+            trackRow.dataset.length = idvTrack.length
+            trackRow.dataset.epname = epName;
+            trackRow.dataset.albumurl = epArt;
             trackRow.addEventListener("click", handleTrackSelect);
             trackRow.classList.add("t-row")
             tTitle.classList.add("album-track");
