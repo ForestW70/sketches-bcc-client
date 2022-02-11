6ff21ad5e96768d4a34850aec9b345a976d2a2cc
Forest wilson <hexaforest@gmail.com>
2022-02-11 17:49:16 -0500

    creating render functions

--- 2.0playground/index.js
+++ 2.0playground/index.js
@@ -1,6 +1,9 @@
 import { sketches } from "./sketches.js";
 
 (function () {
+    // some paths to append too
+
+    // handle list of all seperate tracks
     const songList = []
     const converter = (albumArr) => {
         albumArr.map((ep) => {
@@ -24,68 +27,131 @@ import { sketches } from "./sketches.js";
         })
     }
 
-    const popSongs = () => {
-        const pushCont = document.getElementById("disco")
-        const listFrag = document.createDocumentFragment();
 
-        songList.map((song, idx) => {
-            const papaDiv = document.createElement("div");
-            papaDiv.dataset.title = song.songTitle;
-            papaDiv.dataset.artist = song.epArtist;
-            papaDiv.dataset.length = song.songLength;
-            papaDiv.dataset.epName = song.epName;
-            papaDiv.dataset.artUrl = song.epArtLink;
-            papaDiv.dataset.trkUrl = song.songUrl;
-            papaDiv.dataset.ogFile = song.songOgFile;
-            papaDiv.dataset.daw = song.songdaw;
-            papaDiv.dataset.dCreated = song.songStDate;
-            papaDiv.dataset.tCreated = song.songStTime;
-            papaDiv.addEventListener("click", (e) => {
-                e.preventDefault();
-                const search = e.currentTarget.dataset.trkUrl
-                console.log(search);
+    // render music
+    const displaySongs = (tTitle, artist, tLen, epName, epArt, tUrl, tOgFile, tDaw, tDate, tTime, view, index="") => {
+        // song Row
+        const papaDiv = document.createElement("div");
+        papaDiv.dataset.title = tTitle;
+        papaDiv.dataset.artist = artist;
+        papaDiv.dataset.length = tLen;
+        papaDiv.dataset.epName = epName;
+        papaDiv.dataset.trkUrl = tUrl;
+        papaDiv.dataset.ogFile = tOgFile;
+        papaDiv.dataset.daw = tDaw;
+        papaDiv.dataset.dCreated = tDate;
+        papaDiv.dataset.tCreated = tTime;
+        papaDiv.dataset.artUrl = epArt;
 
-                document.getElementById("player").src = `https://forestw70.github.io/sketches-bcc-client/assets/music/${search}.mp3`
-            })
+        papaDiv.addEventListener("click", (e) => {
+            e.preventDefault();
+            const search = e.currentTarget.dataset.trkUrl
+            console.log(search);
 
+            document.getElementById("player").src = `https://forestw70.github.io/sketches-bcc-client/assets/music/${search}.mp3`
+        })
 
-            const artistSp = document.createElement("span");
-            const epSp = document.createElement("span");
-            const ttlSp = document.createElement("span");
-            const LenSp = document.createElement("span");
-            const urlSp = document.createElement("span");
-            const ogSp = document.createElement("span");
-            const dawSp = document.createElement("span");
-            const dteSp = document.createElement("span");
-            const tmeSp = document.createElement("span");
+        // row items
+        const trkNum = document.createElement("span");
+        const ttlSp = document.createElement("span");
+        const artistSp = document.createElement("span");
+        const LenSp = document.createElement("span");
+        const epSp = document.createElement("span");
+        const urlSp = document.createElement("span");
+        const ogSp = document.createElement("span");
+        const dawSp = document.createElement("span");
+        const dteSp = document.createElement("span");
+        const tmeSp = document.createElement("span");
 
-            artistSp.innerText = song.epArtist
-            epSp.innerText = song.epName
-            ttlSp.innerText = song.songTitle
-            LenSp.innerText = song.songLength
-            urlSp.innerText = song.songUrl
-            ogSp.innerText = song.songOgFile
-            dawSp.innerText = song.songDaw
-            dteSp.innerText = song.songStDate
-            tmeSp.innerText = song.songStTime
+        trkNum.innerText = index;
+        ttlSp.innerText = tTitle;
+        artistSp.innerText = artist;
+        LenSp.innerText = tLen;
+        epSp.innerText = epName;
+        urlSp.innerText = tUrl;
+        ogSp.innerText = tOgFile;
+        dawSp.innerText = tDaw;
+        dteSp.innerText = tDate;
+        tmeSp.innerText = tTime;
 
-            papaDiv.appendChild(artistSp)
-            papaDiv.appendChild(epSp)
+        if (view === "album") {
+            papaDiv.appendChild(trkNum)
             papaDiv.appendChild(ttlSp)
+            // papaDiv.appendChild(artistSp)
             papaDiv.appendChild(LenSp)
+            // papaDiv.appendChild(epSp)
+            papaDiv.appendChild(urlSp)
+            // papaDiv.appendChild(ogSp)
+            papaDiv.appendChild(dawSp)
+            // papaDiv.appendChild(dteSp)
+            papaDiv.appendChild(tmeSp)
+
+            
+        } else {
+            papaDiv.appendChild(trkNum)
+            papaDiv.appendChild(ttlSp)
+            papaDiv.appendChild(artistSp)
+            papaDiv.appendChild(LenSp)
+            papaDiv.appendChild(epSp)
             papaDiv.appendChild(urlSp)
             papaDiv.appendChild(ogSp)
             papaDiv.appendChild(dawSp)
             papaDiv.appendChild(dteSp)
             papaDiv.appendChild(tmeSp)
+        }
 
-            listFrag.appendChild(papaDiv);
-        })
-        pushCont.appendChild(listFrag);
+        return papaDiv;
     }
+
+    const trackListDump = document.getElementById("disco")
+
+    // album view
+    const swap2AlbumView = () => {
+        trackListDump.innerText = "";
+        renderAlbumList();
+    }
+
+    const renderAlbumList = () => {
+        const listFrag = document.createDocumentFragment();
+
+        sketches.map(ep => {
+            ep.trackList.map((song, idx) => {
+               const songRow = displaySongs(song.track, ep.artist, song.length, ep.title, ep.webLink, song.url, song.fileName, song.daw, song.date, song.started, "album", idx);
+               listFrag.appendChild(songRow);
+
+            })
+        })
+        trackListDump.appendChild(listFrag)
+    }
+
+    // song list view
+    const swap2ListView = () => {
+        trackListDump.innerText = "";
+        renderSongList();
+    }
+
+    const renderSongList = () => {
+        const listFrag = document.createDocumentFragment();
+
+        songList.map(song => {
+            const songRow = displaySongs(song.songTitle, song.epArtist, song.songLength, song.epName, song.epArtLink, song.songUrl, song.songOgFile, song.songDaw, song.songStDate, song.songStTime, "list", 0);
+            listFrag.appendChild(songRow);
+        })
+        trackListDump.appendChild(listFrag);
+    }
+
+
+    // buttons
+    const avButton = document.getElementById("viewAlbums")
+    const slButton = document.getElementById("viewSongs")
+
+    avButton.addEventListener("click", () => swap2AlbumView());
+    slButton.addEventListener("click", () => swap2ListView());
+
+
 
 
 
     converter(sketches);
-    popSongs();
+    renderAlbumList();
 })()
